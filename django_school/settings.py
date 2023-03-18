f38ff2e2c1ca9b689e242986db821edd1637c935
Nischith MK <55983139+nischithmk@users.noreply.github.com>
2023-03-18 13:00:56 +0530

    commented static root line

--- django_school/settings.py
+++ django_school/settings.py
@@ -109,7 +109,7 @@ CSRF_TRUSTED_ORIGINS = [
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
